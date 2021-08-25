554d0ba69f5e486cc5bf80fbd242dbab389b1b92
Lien Pham <13520439.uit@gmail.com>
2021-08-25 15:10:50 +0700

    Create automation framework

---
+++ testcases/com/nopcommerce/user/User_01_Register_Login.java
@@ -0,0 +1,35 @@
+package com.nopcommerce.user;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class User_01_Register_Login {
+
+	@BeforeClass
+	public void beforeClass() {
+		
+	}
+
+	@Test
+	public void User_01_Register_To_System() {
+		
+	}
+	
+	@Test
+	public void User_02_Login_To_System() {
+		
+	}
+	
+	@Test
+	public void User_03_Verify_My_Account() {
+		
+	}
+  
+
+	@AfterClass
+	public void afterClass() {
+		
+	}
+
+}
