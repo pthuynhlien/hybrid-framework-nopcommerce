3a56b1e7b9163c4668f959a0a3e1f4458f7ef1b4
Lien Pham <13520439.uit@gmail.com>
2021-10-09 22:07:40 +0700

    Building BasePage class

---
+++ javaTester/eclipseTips/Topic_01_Template.java
@@ -0,0 +1,17 @@
+package eclipseTips;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+public class Topic_01_Template {
+	static WebDriver driver = null;
+
+	public static void main(String[] args) {
+		// WebElement field = driver.findElement(By.xpath(""));
+
+		// Assert.assertTrue(false);
+	}
+
+}
